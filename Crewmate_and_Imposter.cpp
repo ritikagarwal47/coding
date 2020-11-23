55d9f31cdac9b5a33df89f21ab1799c16baa71c3
ritikagarwal47 <ritikagarwal470@gmail.com>
2020-11-24 01:54:54 +0530

    cc

--- Crewmate_and_Imposter.cpp
+++ Crewmate_and_Imposter.cpp
@@ -5,26 +5,18 @@ const int mod=1e9+7;
 // I'm in Love with Experience ^_^
 ll fun(ll N)
 {
-    vector<ll> V;
+    //vector<ll> V;
+    ll ans=0;
     for(ll i=1;i*i<=N;++i)
     {
-        if(N%i==0)
-        {
-            ll a = i;
-            ll b = N/i;
-            if(a!=b)
-            {
-                V.push_back(a);V.push_back(b);
-            }
-            else V.push_back(a);
-        }
+        if(i%3!=0)++ans;
     }
-    return (ll)V.size();
+    return ans;
 }
 void solve()
 {
     ll N;cin>>N;
-    cout<< fun(N)/2 <<'\n';
+    cout<< fun(N) <<'\n';
 }
 int main(void)
 {
