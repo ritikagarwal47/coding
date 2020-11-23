b9c05979aa029e6a250364a318851775fd0055b0
ritikagarwal47 <ritikagarwal470@gmail.com>
2020-11-24 01:55:11 +0530

    HE nov 20

---
+++ Permutations_of_numbers.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+const int mod=1e9+7;
+// I'm in Love with Experience ^_^
+void solve()
+{
+    ll N;cin>>N;
+    ll A[N];for(ll i=0;i<N;++i)cin>>A[i];
+    ll B[N];for(ll i=0;i<N;++i)cin>>B[i];
+    sort(B,B+N);sort(A,A+N,greater<ll>());
+    ll ans=0;
+    for(ll i=0;i<N;++i)
+    {
+        ans += (A[i]*i + B[i]*(N-i-1));
+    }
+    cout<<ans<<'\n';
+}
+int main(void)
+{
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int T=1;        cin>>T;
+while(T--){solve();}exit(0);
+}/*Solved By:- Ritik Agarwal*/
