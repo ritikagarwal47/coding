e0c80bac85d3586ed37d7c29afe8325ec7fcdab7
ritikagarwal47 <ritikagarwal470@gmail.com>
2020-12-17 02:30:12 +0530

    cc

--- Sabotage.cpp
+++ Sabotage.cpp
@@ -2,22 +2,26 @@
 #define ll long long
 using namespace std;
 const int mod=1e9+7;
+ll max_val = 1e18 + 1LL;
 // I'm in Love with Experience ^_^
 vector<ll> pr;
 void SieveOfEratosthenes(ll n) 
 { 
     bool prime[n+1]; 
     memset(prime, true, sizeof(prime)); 
-  
-    for (ll p=2; p*p<=n; p++) 
-    { 
-        if (prime[p] == true) 
-        { 
-            for (ll i=p*p; i<=n; i += p) prime[i] = false; 
-        } 
-    } 
-    for (ll p=2; p<=n; p++) 
-       if (prime[p]) pr.push_back((ll)pow(p,4));
+    for(ll p=2;p<n;++p)
+    {
+        ll mul = 2;
+        if(!prime[p])continue;
+        while(p * mul < n)
+        {
+            prime[p * mul] = false;mul ++;
+        }
+        ll m = (ll)p;
+        if(m*m*m > (max_val / m))
+            break;
+        pr.push_back(m*m*m*m);
+    }
 } 
 void solve()
 {
@@ -29,7 +33,7 @@ void solve()
 int main(void)
 {
 ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-SieveOfEratosthenes(9000); 
+SieveOfEratosthenes(100000); 
 int T=1;        cin>>T;
 while(T--){solve();}exit(0);
 }/*Solved By:- Ritik Agarwal*/
