27fc98fc2c483a163cca259f04cf2521820d45a4
ritikagarwal47 <ritikagarwal470@gmail.com>
2020-12-29 23:08:34 +0530

    cc

---
+++ The_Begining_Era_Of_Cyberverse.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+const int mod=1e9+7;
+// I'm in Love with Experience ^_^
+void solve()
+{
+    ll N,K;cin>>N>>K;
+    cout<< K/N<<"\n";
+}
+int main(void)
+{
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int T=1;        cin>>T;
+while(T--){solve();}exit(0);
+}/*Solved By:- Ritik Agarwal*/
