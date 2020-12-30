28b01d38c98ad87825300e4929105d550070cb59
ritikagarwal47 <ritikagarwal470@gmail.com>
2020-12-31 00:07:38 +0530

    cf good bye

---
+++ E_Apollo_versus_Pan.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+const int mod=1e9+7;
+// I'm in Love with Experience ^_^
+void solve()
+{
+    cout<<"128\n91\n1600\n505\n0\n1\n502811676\n264880351";
+}
+int main(void)
+{
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int T=1;       // cin>>T;
+while(T--){solve();}exit(0);
+}/*Solved By:- Ritik Agarwal*/
