75692ec39827d46945a0a973237d7b65bc4ebec4
ritikagarwal47 <ritikagarwal470@gmail.com>
2021-01-04 22:29:05 +0530

    cf

--- C_Vanya_and_Exams.cpp
+++ C_Vanya_and_Exams.cpp
@@ -5,11 +5,30 @@ const int mod=1e9+7;
 //The end result of coders personal growth is,there codes becomes there documentation
 void solve()
 {
-    
+    ll N,M,avg;cin>>N>>M>>avg;
+    vector<pair<ll,ll>> V;
+    ll r=avg*N;
+    for(ll i=0;i<N;++i)
+    {
+        ll x,y;cin>>x>>y;
+        V.push_back({y,x});
+        r-=x;
+    }
+    sort(V.begin(),V.end());
+    ll ans=0,a=0;
+    while(r>0)
+    {
+        ll x = min(r,M-V[a].second);
+        ans += x*V[a].first;
+        ++a;
+        r-=x;
+    }
+    cout<< ans <<'\n';
+
 }
 int main(void)
 {
 ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-int T=1;        cin>>T;
+int T=1;        //cin>>T;
 while(T--){solve();}exit(0);
 }/*Solved By:- Ritik Agarwal*/
