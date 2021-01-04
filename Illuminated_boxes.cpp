a5f64497c783c249e5437f388042bb215a803658
ritikagarwal47 <ritikagarwal470@gmail.com>
2021-01-04 10:00:27 +0530

    cc

--- Illuminated_boxes.cpp
+++ Illuminated_boxes.cpp
@@ -3,26 +3,10 @@
 using namespace std;
 const int mod=1e9+7;
 //The end result of coders personal growth is,there codes becomes there documentation
-ll power(ll x,ll y)  
-{  
-    ll res = 1;  
-  
-    x = x % mod; 
-    if (x == 0) return 0; 
-  
-    while (y > 0)  
-    {  
-        if (y & 1)  res = (res*x) % mod;  
-        y = y>>1;
-        x = (x*x) % mod;  
-    }  
-    return res;  
-}
 void solve()
 {
     ll N,M;cin>>N>>M;
-    N/=2;//N++;
-    cout<< power(M,(N*(N+1)))<<'\n';
+    cout<< (N*M)/__gcd(N,M) <<'\n';
 }
 int main(void)
 {
