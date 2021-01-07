f31491adf6bc6cad03df15a45c664575688e4d6e
ritikagarwal47 <ritikagarwal470@gmail.com>
2021-01-08 01:24:57 +0530

    cf 582

---
+++ B_Bad_Prices.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+const int mod=1e9+7;
+// I'm in Love with Experience ^_^
+void solve()
+{
+    ll N;cin>>N;
+    ll A[N]; for(ll i=0;i<N;++i)cin>>A[i];
+    ll mn=A[N-1],ans=0;
+    for(ll i=N-1;i>=0;--i)
+    {
+        if(A[i]>mn)++ans;
+        mn = min(mn,A[i]);
+    }
+    cout<<ans<<'\n';
+}
+int main(void)
+{
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int T=1;        cin>>T;
+while(T--){solve();}exit(0);
+}/*Solved By:- Ritik Agarwal*/
