5ffeb9195b7da49bcd8bdffc150265d4fb08146e
ritikagarwal47 <ritikagarwal470@gmail.com>
2021-01-08 22:28:20 +0530

    cc

---
+++ Different_Colored_Pens.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+const int mod=1e9+7;
+//The end result of coders personal growth is,there codes becomes there documentation
+void solve()
+{
+    ll N;cin>>N;
+    string s;cin>>s;
+    ll ans=0;
+    for(ll i=0;i<N-1;++i)
+    {
+        if(s[i]==s[i+1])++ans;
+    }
+    cout<<ans<<'\n';
+}
+int main(void)
+{
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int T=1;        cin>>T;
+while(T--){solve();}exit(0);
+}/*Solved By:- Ritik Agarwal*/
