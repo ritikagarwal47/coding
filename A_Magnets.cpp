6a105d0bce6ad2d7bb99e9ae42e84a6c1f691551
ritikagarwal47 <ritikagarwal470@gmail.com>
2021-01-14 20:04:50 +0530

    cf

---
+++ A_Magnets.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+const int mod=1e9+7;
+//The end result of coders personal growth is,there codes becomes there documentation
+void solve()
+{
+    ll N;cin>>N;
+    ll ans=0;++ans;
+    ll l,x;cin>>l;
+    while(N--)
+    {
+        cin>>x;
+        if(l%10==x/10)++ans;
+        l=x;
+    } 
+    cout<<ans<<'\n';
+}
+int main(void)
+{
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int T=1;        //cin>>T;
+while(T--){solve();}exit(0);
+}/*Solved By:- Ritik Agarwal*/
---
+++ A_Word_Capitalization.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+const int mod=1e9+7;
+//The end result of coders personal growth is,there codes becomes there documentation
+void solve()
+{
+    string s;cin>>s;
+    if(islower(s[0]))
+    {
+        //toupper(s[0]);
+        transform(s.begin(),s.begin()+1,s.begin(),::toupper);
+    }
+    cout<<s<<'\n';
+}
+int main(void)
+{
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int T=1;        //cin>>T;
+while(T--){solve();}exit(0);
+}/*Solved By:- Ritik Agarwal*/
