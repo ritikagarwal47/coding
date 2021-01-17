405db037b6b4b15462270bc51fa58af64513767a
ritikagarwal47 <ritikagarwal470@gmail.com>
2021-01-17 18:34:09 +0530

    cc

---
+++ F_D.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+const int mod=1e9+7;
+//The end result of coders personal growth is,there codes becomes there documentation
+void solve()
+{
+    ll N;cin>>N;
+    if(N==1)
+    {
+        cout<<"1\n";return;
+    }
+    ll a = 2*N-1;
+    ll b=1;
+    ll p=0;
+    for(ll i=0;i<N;++i)
+    {
+        b=1;
+        for(ll j=0;j<i;++j)
+        {
+            cout<<' ';++b;
+        }
+        for(ll j=0;j<a;++j)
+        {
+            if(i==0)cout<<b;
+            else if(i&1 and b%2==0)cout<<b;
+            else if(i%2==0 and b&1)cout<<b;
+            else cout<<' ';
+            ++b;
+        }
+        a-=2;
+        cout<<'\n';
+        p =N-i;
+    }
+    a=3;
+    for(ll i=0;i<N-1;++i)
+    {
+        b=1;
+        for(ll j=0;j<N-i-2;++j)
+        {
+            cout<<' ';++b;
+        }
+        for(ll j=0;j<a;++j)
+        {
+            if(i==N-2)cout<<b;
+            else if(i&1 and b&1)cout<<b;
+            else if(i%2==0 and b%2==0)cout<<b;
+            else cout<<' ';
+            ++b;
+        }
+        a+=2;
+        cout<<'\n';
+    }
+    //cout<<'\n';
+}
+int main(void)
+{
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int T=1;        cin>>T;
+while(T--){solve();}exit(0);
+}/*Solved By:- Ritik Agarwal*/
