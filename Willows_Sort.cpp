60d19932d2842349ee41f4849560acaa20079f35
ritikagarwal47 <ritikagarwal470@gmail.com>
2021-01-19 00:11:32 +0530

    cc

--- Willows_Sort.cpp
+++ Willows_Sort.cpp
@@ -4,36 +4,23 @@ using namespace std;
 const int mod=1e9+7;
 //The end result of coders personal growth is,there codes becomes there documentation
 ll t=1;
-bool cmp(ll a,ll b)
-{
-    ll x = a%10;
-    ll y = b%10;
-   while(x==y and x!=0 and y!=0)
-   {
-       a/=10;b/=10;
-       x%=10;y%=10;
-   }
-   return x<y;
-}
 void solve()
 {
     ll N;cin>>N;
-    //ll A[N];for(ll i=0;i<N;++i)cin>>A[i];
-    vector<ll> adj[10];
+    vector<string> V;
     for(ll i=0;i<N;++i)
     {
-        ll x;cin>>x;
-        adj[x%10].push_back(x);
+        string x;cin>>x;
+        reverse(x.begin(),x.end());
+        V.push_back(x);
     }
-    for(ll i=0;i<10;++i)
-    {
-        sort(adj[i].begin(),adj[i].end(),cmp);
-    }
+    sort(V.begin(),V.end());
     cout<<"Case "<<t++<<":\n";
-    //for(ll i=0;i<N;++i)cout<<A[i]<<'\n';
-    for(ll i=0;i<10;++i)
+    for(ll i=0;i<N;++i)
     {
-        for(auto j : adj[i])cout<<j<<'\n';
+        string x = V[i];
+        reverse(x.begin(),x.end());
+        cout<<x<<'\n';
     }
 }
 int main(void)
