b164e32bf222253d03fc7fcedaad1f6bab98db07
ritikagarwal47 <ritikagarwal470@gmail.com>
2021-01-31 22:19:36 +0530

    cc

---
+++ Magical_Game.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+const int mod=1e9+7;
+//The end result of coders personal growth is,there codes becomes there documentation
+void solve()
+{
+    ll N;cin>>N;
+    ll c=0;
+    while(N)
+    {
+        if(N&1)++c;
+        N>>=1;
+    } 
+    if(c%2==0)cout<<"Magical\n";
+    else cout<<"Non-Magical\n";
+}
+int main(void)
+{
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int T=1;        cin>>T;
+while(T--){solve();}exit(0);
+}/*Solved By:- Ritik Agarwal*/
