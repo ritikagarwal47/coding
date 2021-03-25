9f9021be4ae2f29f23d647771a28446ca12ea8e9
ritikagarwal47 <ritikagarwal470@gmail.com>
2021-03-25 11:47:23 +0530

    cf 709

--- A_Prison_Break.cpp
+++ A_Prison_Break.cpp
@@ -5,13 +5,8 @@ const int mod=1e9+7;
 // I'm in Love with Experience ^_^
 void solve()
 {
-    ll n,m,r,c;cin>>n>>m>>r>>c;
-    ll m1 = max(n-r + m -c, r - 1 + c - 1);
-    ll m2 = max(abs(n-1) + abs(m-c),abs(n-r) + abs(m-1));
-   // cout<< max(m1,m2) <<'\n';  
-   //cout<< max(n-r,r-1) << ' '<<max(m-c,c-1)<<'\n';
-    cout<< max(n-r,r-1) + max(m-c,c-1)<<'\n';
-    //m + n - r + c
+    ll a,b;cin>>a>>b;
+    cout<< (a*b) <<'\n';
 }
 int main(void)
 {
