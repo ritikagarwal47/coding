0c2ab93293c21544d937a03e1de0a676ef8d445f
ritikagarwal47 <ritikagarwal470@gmail.com>
2021-04-22 22:16:39 +0530

    cf 146

--- A_LCM_Challenge.cpp
+++ A_LCM_Challenge.cpp
@@ -3,17 +3,14 @@
 using namespace std;
 const int mod=1e9+7;
 //The end result of coders personal growth is,there codes becomes there documentation
-ll lcm(ll a,ll b)
-{
-    return (a / __gcd(a, b)) * b;
-}
 void solve()
 {
     ll N;cin>>N;
-   // ll a=N,b=N-1,c=N-2;
     if(N==1)cout<<"1";
     else if(N==2)cout<<"2";
-    else cout<< max(lcm(lcm(N,N-1),N-2),max(lcm(lcm(N-1,N-2),N-3),lcm(lcm(N-2,N-3),N-4))) <<'\n';
+    else if(N&1)cout<<N*(N-1)*(N-2)<<'\n';
+    else if(__gcd(N,N-3)==1)cout<<N*(N-1)*(N-3)<<'\n';
+    else cout<<(N-1)*(N-2)*(N-3)<<'\n';
 }
 int main(void)
 {
