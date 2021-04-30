24de9799174515a2415304411f2f4065a44cf941
ritikagarwal47 <ritikagarwal470@gmail.com>
2021-04-30 18:17:41 +0530

    hwi sample test

--- infytq_sample.cpp
+++ infytq_sample.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 const int mod=1e9+7;
 //The end result of coders personal growth is,there codes becomes there documentation
-ll dp[10000][10000];
+// ll dp[10000][10000];
 ll N;
-ll fun(ll A[],ll l,ll r,ll c,ll X)
-{
-    //cout<<X<<' ';
-    if(X<=0)return c;
-    if(l>r)return N;
-    if(dp[l][r]!=-1)return dp[l][r];
-    if(l==r)return dp[l][r] = fun(A,l+1,r,c+1,X-A[l]);
-    return dp[l][r] = min(fun(A,l+1,r,c+1,X-A[l]),fun(A,l,r-1,c+1,X-A[r]));
-}
+// ll fun(ll A[],ll l,ll r,ll c,ll X)
+// {
+//     //cout<<X<<' ';
+//     if(X<=0)return c;
+//     if(l>r)return N;
+//     if(dp[l][r]!=-1)return dp[l][r];
+//     if(l==r)return dp[l][r] = fun(A,l+1,r,c+1,X-A[l]);
+//     return dp[l][r] = min(fun(A,l+1,r,c+1,X-A[l]),fun(A,l,r-1,c+1,X-A[r]));
+// }
 void solve()
 {
     cin>>N;
@@ -24,10 +24,27 @@ void solve()
     {
         cout<<"-1\n";return;
     }
-    for(ll i=0;i<=N;++i)
-        for(ll j=0;j<=N;++j)
-            dp[i][j]=-1;
-    cout<< fun(A,0,N-1,0,X) <<'\n';
+    ll p = s-X;
+    map<ll,ll> M;
+    ll a=0,l=0;
+    for(ll i=0;i<N;++i)
+    {
+        a+=A[i];
+        if(a==p)l = i+1;
+
+        if(M.find(a)==M.end())
+        {
+            M[a]=i;
+        }
+
+        if(M.find(a-p)!=M.end())
+        {
+            l = max(l,i - M[a-p]);
+        }
+
+    }
+    if(l==0)cout<<"-1\n";
+    else cout<< N - l <<'\n'; 
 }
 int main(void)
 {
