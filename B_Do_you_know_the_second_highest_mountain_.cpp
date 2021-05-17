4f098fe55a6b2cf0c2e73dd9d9f6edf4ff8613d8
ritikagarwal47 <ritikagarwal470@gmail.com>
2021-05-17 22:52:17 +0530

    cf

---
+++ B_Do_you_know_the_second_highest_mountain_.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+const int mod=1e9+7;
+//The end result of coders personal growth is,there codes becomes there documentation
+void solve()
+{
+    ll N;cin>>N;
+    vector<pair<ll,string>> V;
+    while(N--)
+    {
+        string s;ll t;cin>>s>>t;V.push_back({t,s});
+    }
+    sort(V.begin(),V.end());
+    cout<<V[V.size()-2].second<<'\n';
+}
+int main(void)
+{
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int T=1;       // cin>>T;
+while(T--){solve();}exit(0);
+}/*Solved By:- Ritik Agarwal*/
